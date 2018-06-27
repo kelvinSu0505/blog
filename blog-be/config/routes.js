8757d87f23ca1795f7d8a898044a94bef415196a
duyongqiang <duyongqiang@ezhaoge.com>
2018-06-27 16:33:15 +0800

    updata routes.js edit

--- blog-be/config/routes.js
+++ blog-be/config/routes.js
@@ -2,7 +2,7 @@
  * @Author: Yorke 
  * @Date: 2018-06-27 14:27:50 
  * @Last Modified by: Yorke
- * @Last Modified time: 2018-06-27 16:30:23
+ * @Last Modified time: 2018-06-27 16:31:52
  * 路由配置
  */
 
@@ -10,11 +10,11 @@
  
 // 引入单独处理对应逻辑的文件
 
-var Index = require('../controllers/index');               // 首页相关逻辑
-var User = require('../controllers/user');                 // 用户相关逻辑
-var Essay = require('../controllers/essay');               // 文章相关逻辑
-var About = require('../controllers/about');               // 关于相关逻辑
-var Other = require('../controllers/other');               // 其他功能相关逻辑
+var Index = require('../controllers/index.js');               // 首页相关逻辑
+var User = require('../controllers/user.js');                 // 用户相关逻辑
+var Essay = require('../controllers/essay.js');               // 文章相关逻辑
+var About = require('../controllers/about.js');               // 关于相关逻辑
+var Other = require('../controllers/other.js');               // 其他功能相关逻辑
 
 module.exports = function (app){
     /*
