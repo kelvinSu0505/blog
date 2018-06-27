0eb61d409e31db538a30acd4c7c372569a56fe6b
duyongqiang <duyongqiang@ezhaoge.com>
2018-06-27 16:31:26 +0800

    updata add about.js and routes.js edit

--- blog-be/config/routes.js
+++ blog-be/config/routes.js
@@ -2,11 +2,32 @@
  * @Author: Yorke 
  * @Date: 2018-06-27 14:27:50 
  * @Last Modified by: Yorke
- * @Last Modified time: 2018-06-27 14:30:29
+ * @Last Modified time: 2018-06-27 16:30:23
  * 路由配置
  */
+
+
+ 
 // 引入单独处理对应逻辑的文件
-var Index = require('../controllers/index');
-var User = require('../controllers/user');
-var Essay = require('../controllers/essay');
-var Other = require('../controllers/other');
+
+var Index = require('../controllers/index');               // 首页相关逻辑
+var User = require('../controllers/user');                 // 用户相关逻辑
+var Essay = require('../controllers/essay');               // 文章相关逻辑
+var About = require('../controllers/about');               // 关于相关逻辑
+var Other = require('../controllers/other');               // 其他功能相关逻辑
+
+module.exports = function (app){
+    /*
+        注释:  
+    */
+
+
+    // Index 
+    app.get('/index',Index.index);                      // 博客首页
+
+    // admin user
+    app.get('/blog/login',User.login);                  // admin管理页面登录
+
+    // Essay 
+    
+}
---
+++ blog-be/controllers/about.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: Yorke 
+ * @Date: 2018-06-27 16:29:50 
+ * @Last Modified by: Yorke
+ * @Last Modified time: 2018-06-27 16:30:21
+ * 关于相关接口逻辑处理
+ */
